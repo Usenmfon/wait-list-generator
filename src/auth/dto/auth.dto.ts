4cdc70cd85c59924d6555bdb7c628e8abc685940
Usenmfon <usenmfonuko@gmail.com>
2023-04-11 12:34:31 +0100

    updated auth dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsObject, IsString } from 'class-validator';
 
 export class SignUpDto {
   @IsNotEmpty()
@@ -12,4 +12,34 @@ export class SignUpDto {
   @IsString()
   @IsNotEmpty()
   password: string;
+}
+
+export class SignInDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class RefreshTokenDto {
+  @IsNotEmpty()
+  refreshToken: string;
+}
+
+export class SignUpHookDto {
+  @IsObject()
+  event: { name: string; type: string };
+
+  @IsObject()
+  data: {
+    created_at?: string;
+    email?: string;
+    email_verified?: string;
+    fullname?: string;
+    last_password_reset?: string;
+    updated_at?: string;
+  };
 }
