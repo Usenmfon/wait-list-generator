e8d7aa8697e947a755c22431b1a7dd60bd08a2e0
Usenmfon <usenmfonuko@gmail.com>
2023-04-16 20:32:03 +0100

    updated list resource

--- src/system/modules/user.module.ts
+++ src/system/modules/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ProfileModule } from '../resources/profile/profile.module';
+import { ListModule } from '../resources/list/list.module';
 
-@Module({ imports: [ProfileModule] })
+@Module({ imports: [ProfileModule, ListModule] })
 export class UserModule {}
