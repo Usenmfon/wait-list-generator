8b81e283629f5ab63a8897099e54cebb3edc8a69
Usenmfon <usenmfonuko@gmail.com>
2023-04-22 11:43:27 +0100

    image upload complete

--- src/system/resources/list/list.module.ts
+++ src/system/resources/list/list.module.ts
@@ -4,6 +4,7 @@ import { ListService } from './list.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/auth/schema/auth.schema';
 import { List, ListSchema } from './schema/list.schema';
+import { CloudinaryModule } from 'src/helper/cloudinary/cloudinary.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { List, ListSchema } from './schema/list.schema';
       { name: User.name, schema: UserSchema },
       { name: List.name, schema: ListSchema },
     ]),
+    CloudinaryModule,
   ],
   controllers: [ListController],
   providers: [ListService],
